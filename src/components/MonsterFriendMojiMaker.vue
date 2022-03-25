e04f3be598fb99ccc1eb30f948845577d950ed4c
Erran Carey <me@errancarey.com>
2022-03-25 00:05:45 +0000

    Initial commit of vue create template

---
+++ src/components/MonsterFriendMojiMaker.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <p>Made with ❤️ by erran. Inspired by a <a href="https://twitter.com/mintrainmint/status/1507086119703355402?s=20&t=4Po1jh6vasgpwgN45kDBLg">cool Tweet by @mintrain</a>.</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
